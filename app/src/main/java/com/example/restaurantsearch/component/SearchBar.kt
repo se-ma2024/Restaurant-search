969e4fcb4dcca4efdc9107890291658c43d0c05c
se-ma2024 <Sei.2024.IT@gmail.com>
2023-10-13 01:24:07 +0900

    検索バーのキーボード追加

--- app/src/main/java/com/example/restaurantsearch/component/SearchBar.kt
+++ app/src/main/java/com/example/restaurantsearch/component/SearchBar.kt
@@ -3,6 +3,8 @@ package com.example.restaurantsearch.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -16,6 +18,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.zIndex
@@ -40,7 +44,16 @@ fun SearchBar(onSearch: (String) -> Unit) {
                         text = it
                     },
                     placeholder = { Text("検索...") },
-                    modifier = Modifier.weight(1f)
+                    modifier = Modifier.weight(1f),
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Search//エンターキーを検索アクションにマップ
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onSearch = {
+                            onSearch(text.text)
+                        }
+                    )
                 )
                 Button(
                     onClick = {
