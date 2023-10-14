25dac5617ff6f45e1ffc4a7f8013d33bbbe24fef
se-ma2024 <Sei.2024.IT@gmail.com>
2023-10-15 06:04:41 +0900

    SearchResultCard微調整

--- app/src/main/java/com/example/restaurantsearch/component/SearchResultCard.kt
+++ app/src/main/java/com/example/restaurantsearch/component/SearchResultCard.kt
@@ -1,10 +1,12 @@
 package com.example.restaurantsearch.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,6 +15,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -28,11 +31,16 @@ fun SearchResultCard() {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .height(200.dp)
-            .padding(8.dp),
+            .height(180.dp)
+            .padding(start = 12.dp, top = 4.dp, end = 12.dp, bottom = 4.dp)
+            .background(color = Color.White),
     ) {
-        Column() {
-            Spacer(modifier = Modifier.height(16.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .background(Color.White)
+        ) {
+            Spacer(modifier = Modifier.height(12.dp))
             Row(horizontalArrangement = Arrangement.Center) {
                 Spacer(modifier = Modifier.width(16.dp))
                 Text(
@@ -42,7 +50,7 @@ fun SearchResultCard() {
             }
             Spacer(modifier = Modifier.height(8.dp))
             Row(modifier = Modifier.padding(end = 16.dp)) {
-                Spacer(modifier = Modifier.width(16.dp))
+                Spacer(modifier = Modifier.width(12.dp))
                 Image(
                     painter = painterResource(id = R.drawable.ic_launcher_background),
                     contentDescription = null
